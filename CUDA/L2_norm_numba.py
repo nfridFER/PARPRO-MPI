defa18756b2ae3fe474bd6b97a2fb6550bfaf892
nfridFER <147061222+nfridFER@users.noreply.github.com>
2025-06-02 15:45:11 +0200

    Update L2_norm_numba.py

--- CUDA/L2_norm_numba.py
+++ CUDA/L2_norm_numba.py
@@ -2,29 +2,43 @@
 
 import numpy as np
 import time
-from numba import guvectorize, float32, cuda
+from numba import cuda, float32
 
-@guvectorize([(float32[:], float32[:])], '(n)->()', target='cuda')
-def row_l2norm(x, out):
-    tmp = 0.0
-    for i in range(x.shape[0]):
-        tmp += x[i] * x[i]
-    out[0] = tmp ** 0.5
+@cuda.jit
+def l2norm_kernel(A, out, cols):
+    row = cuda.grid(1)
+    if row < A.shape[0]:
+        tmp = 0.0
+        for i in range(cols):
+            val = A[row, i]
+            tmp += val * val
+        out[row] = tmp ** 0.5
 
-N = 4096
-COLS = 256
-A = np.random.randint(0, 10, size=(N, COLS)).astype(np.float32)
-out = np.zeros(N, dtype=np.float32)
+
+rows = 4096
+cols = 256
+
+A = np.random.randint(0, 10, size=(rows, cols)).astype(np.float32)
+out = np.zeros(rows, dtype=np.float32)
+
+dA = cuda.to_device(A)
+dOut = cuda.device_array(rows, dtype=np.float32)
+
+threads_per_block = 256
+blocks_per_grid = (rows + threads_per_block - 1) // threads_per_block
 
 # Warm-up
-_ = row_l2norm(A)
+l2norm_kernel[blocks_per_grid, threads_per_block](dA, dOut, cols)
 cuda.synchronize()
 
 # Timing
 start = time.time()
-out = row_l2norm(A)
+l2norm_kernel[blocks_per_grid, threads_per_block](dA, dOut, cols)
 cuda.synchronize()
 end = time.time()
 
+out = dOut.copy_to_host()
+
 print(f"L2 norm comp. t: {(end - start)*1000:.2f} ms")
+
 print("Zadnjih 5 L2 normi:", out[-5:])
